0ba44525f9c4ba664f805b93afec45c59edbbca3
Steffen Wesiz <steffen.weisz@capgemini.com>
2024-08-24 12:31:41 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="at-wrap">
+        Our whole component will go into this wrapper
+      </div>
+    </>
+  );
+}
+
+export default App;
